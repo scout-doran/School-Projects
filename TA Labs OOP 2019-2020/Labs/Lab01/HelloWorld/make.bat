59ae2ba11130339118df6e367c7068370ce0dd95
Scout <74687119+scout-doran@users.noreply.github.com>
2021-10-23 23:16:30 -0500

    lab 1

---
+++ TA Labs OOP 2019-2020/Labs/Lab01/HelloWorld/make.bat
@@ -0,0 +1,5 @@
+@echo off             rem silently turns echo off
+cls                   rem clears the console
+
+javac HelloWorld.java rem compiles your program
+java HelloWorld       rem runs your program
